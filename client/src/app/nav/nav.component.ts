919b886be8ce9a40d5f880a126bbc208773c1100
José Cisneros <josecisneros@Joses-MacBook-Pro.local>
2023-03-03 18:18:31 -0600

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -41,7 +41,7 @@ export class NavComponent implements OnInit {
       },
       error: error => {
         //console.log(error);
-        this.toastr.error(error.error);
+        //this.toastr.error(error.error); The interceptor doiung this
       }
     })
   }
